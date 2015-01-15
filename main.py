55e6824297afc86b31ccf728c6e46a7130f3a67b
schlerp <patrick.coffey@live.com.au>
2015-01-15 19:19:21 +0930

    added minor db support using repurposed code from fishtankMon project

--- main.py
+++ main.py
@@ -4,3 +4,13 @@
 # deviceWatcher - main.py
 # -----------------------
 # This is the main program logic
+
+import lib.devices
+import lib.scripts
+import lib.db
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    pass

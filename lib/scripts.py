fb72e839df389eef011f8476c4860b389de02f95
Patrick Coffey <Patrick.Coffey@live.com.au>
2015-01-16 10:37:17 +0930

    added csv parsing

--- lib/scripts.py
+++ lib/scripts.py
@@ -5,3 +5,16 @@
 # ------------------------------
 # This is the classes/functions to
 # Maintain/Manipulate the Devices
+
+import csv
+
+def _parseDeviceCSV(csvFile):
+    csvDevices = csv.reader(csvFile)
+    for row in csvDevices:
+        yield row
+    
+def createDeviceList(csvFile):
+    deviceList = []
+    for device in _parseDeviceCSV(csvFile):
+        deviceList.append(device)
+    return deviceList
